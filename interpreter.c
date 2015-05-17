042e19d15028b8b87d711a5ac58503a4a9a77277
danhan52 <danny.hanson52@gmail.com>
2015-05-16 21:08:18 -0500

    Things are working pretty well! Need to do printing better

--- interpreter.c
+++ interpreter.c
@@ -13,68 +13,157 @@
 #include "talloc.h"
 #include "interpreter.h"
 
-// A frame is a linked list of bindings, and a pointer to another frame.  A
-// binding is a variable name (represented as a string), and a pointer to the
-// Value it is bound to. Specifically how you implement the list of bindings is
-// up to you.
-//struct Frame {
-//    Value *bindings;
-//    struct Frame *parent;
-//};
-
+// finds the symbol within the bindings of the current frame
+// or prints an error and quits
 Value *lookUpSymbol(Value *symb, Frame *frame) {
-    return symb;
-}
-
-Value *evalIf(Value *args, Frame *frame) {
-    if (length(args) < 3) {
-        printf("Interpret error: too few arguments in if\n");
-        texit(1);
-    } else if (length(args) > 3) {
-        printf("Interpret error: too many arguments in if\n");
-        texit(1);
-    } else {
-        Value *cond = eval(car(args), frame);
-        if (cond->type != BOOL_TYPE) {
-            printf("Interpret error: if condition isn't a boolean\n");
+    assert(symb->type == SYMBOL_TYPE);
+    Value *variables = frame->bindings;
+    Value *result;
+    if (variables->type == NULL_TYPE) {     // nothing in bindings
+        Frame *papa = frame->parent;
+        if (papa == NULL) {     // currently in top frame
+            printf("Interpret error: variable reference before declaration\n");
             texit(1);
-        } else {
-            Value *statement;
-            if (cond->i) {
-                statement = car(cdr(args));
-            } else {
-                statement = car(cdr(cdr(args)));
-            }
-            return eval(statement, frame);
+        } else {    // look in parent frame
+            result = lookUpSymbol(symb, papa);
         }
+    } else if (variables->type == CONS_TYPE) {  // bindings has a list
+        Value *curvar = car(variables);
+        variables = cdr(variables);
+        while (1) {     // search bindings
+            if (!strcmp(car(curvar)->s, symb->s)) {    // if the names are the same
+                result = cdr(curvar);
+                break;
+            } else {
+                if (variables->type == NULL_TYPE) { // end of bindings
+                    Frame *papa = frame->parent;
+                    if (papa == NULL) {     // currently in top frame
+                        printf("Interpret error: variable reference before declaration\n");
+                        texit(1);
+                    } else {    // look in parent frame
+                        result = lookUpSymbol(symb, papa);
+                    }
+                } else if (variables->type == CONS_TYPE) {  // more entries
+                    curvar = car(variables);
+                    variables =cdr(variables);
+                } else {    // if I screwed up
+                    printf("Interpret error: frame bindings are funky (bad)\n");
+                    texit(1);
+                }
+            }
+        }
+    } else {    // if I screwed up
+        printf("Interpret error: frame bindings are funky (bad)\n");
+        texit(1);
     }
+    return result;
 }
 
+
+// evaluates if statements or throws an error
+Value *evalIf(Value *args, Frame *frame) {
+    // check length of arguments
+    if (length(args) < 3) {
+        printf("Interpret error (if): too few arguments\n");
+        texit(1);
+    }
+    if (length(args) > 3) {
+        printf("Interpret error (if): too many arguments\n");
+        texit(1);
+    }
+    
+    // get condition
+    Value *cond = eval(car(args), frame);
+    if (cond->type != BOOL_TYPE) {
+        printf("Interpret error (if): condition isn't a boolean\n");
+        texit(1);
+    }
+    
+    // choose return statement
+    Value *statement;
+    if (cond->i) {
+        statement = car(cdr(args));
+    } else {
+        statement = car(cdr(cdr(args)));
+    }
+    return eval(statement, frame);
+}
+
+
+void bindingError() {
+    printf("Interpret error (let): improperly formatted list-of-bindings\n");
+    texit(1);
+}
+
+// evaluates let statement or throws an error
 Value *evalLet(Value *args, Frame *frame) {
-    return args;
+    // empty let
+    if (args->type != CONS_TYPE) {
+        printf("Interpret error (let): no arguments\n");
+        texit(1);
+    }
+    
+    Frame *tempFrame = talloc(sizeof(Frame));
+    tempFrame->parent = frame;
+    Value *variables = car(args);
+    Value *binds = makeNull();
+    if (variables->type == NULL_TYPE) { // no bindings
+    } else if (variables->type == CONS_TYPE) {
+        Value *curvar = car(variables);
+        variables = cdr(variables);
+        
+        while (1) {
+            if (curvar->type != CONS_TYPE) {
+                bindingError();
+            }
+            if (length(curvar) != 2) {
+                bindingError();
+            }
+            
+            Value *varName = car(curvar);
+            if (varName->type != SYMBOL_TYPE) {
+                bindingError();
+            }
+            Value *varVal = eval(car(cdr(curvar)), frame);
+            Value *varBound = cons(varName, varVal);
+            binds = cons(varBound, binds);
+            
+            if (variables->type == NULL_TYPE) {
+                break;
+            } else {
+                curvar = car(variables);
+                variables = cdr(variables);
+            }
+        }
+    } else {
+        bindingError();
+    }
+    
+    tempFrame->bindings = binds;
+    return eval(car(cdr(args)), tempFrame);
 }
 
 
 Value *eval(Value *expr, Frame *frame) {
+    Value *result;
     switch (expr->type) {
         case INT_TYPE:
-            return expr; 
+            result = expr; 
             break;
         case STR_TYPE:
-            return expr;
+            result = expr;
             break;
         case BOOL_TYPE:
-            return expr;
+            result = expr;
             break;
         case DOUBLE_TYPE:
-            return expr;
+            result = expr;
             break;
         case SYMBOL_TYPE:
-            return lookUpSymbol(expr, frame);
+            result = lookUpSymbol(expr, frame);
             break;
         case CONS_TYPE:
         {
-            Value *result;
             Value *first = car(expr);
             Value *args = cdr(expr);
 
@@ -89,7 +178,6 @@ Value *eval(Value *expr, Frame *frame) {
                 printf("Interpret error: unrecognized expression\n");
                 texit(1);
             }
-            return result;
             break;
         }
         default:
@@ -97,6 +185,7 @@ Value *eval(Value *expr, Frame *frame) {
             texit(1);
             break;
     }
+    return result;
 }
 
 void interpret(Value *tree) {
